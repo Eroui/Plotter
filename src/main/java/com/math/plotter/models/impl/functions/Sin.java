2358ec320ea9c61a2b69dd562aa016e3cf5a2641
Abdelaziz EROUI <kapi.abdel@gmail.com>
2018-07-30 18:33:01 +0000

    Implement Sin function

---
+++ src/main/java/com/math/plotter/models/impl/functions/Sin.java
@@ -0,0 +1,16 @@
+package com.math.plotter.models.impl.functions;
+
+import com.math.plotter.models.Expression;
+import com.math.plotter.models.UnaryExpression;
+
+public class Sin extends UnaryExpression {
+
+    public Sin(Expression argument) {
+        super(argument);
+    }
+
+    @Override
+    public double value(double x) {
+        return Math.sin(this.argument.value(x));
+    }
+}
