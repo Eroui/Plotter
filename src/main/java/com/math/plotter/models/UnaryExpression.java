0838766a4d2917685b7229edcd10c0daea7c7a8d
Abdelaziz EROUI <kapi.abdel@gmail.com>
2018-07-31 20:41:12 +0000

    Implement Default Constructor and argument Setter
    > While Analysing a math function which is a UnaryExpression we don't
    know yet what will be wrapped inside the function, thus we need a
    default constructor and a setter to allow consturcting the function and
    assign its argument when ready.

--- src/main/java/com/math/plotter/models/UnaryExpression.java
+++ src/main/java/com/math/plotter/models/UnaryExpression.java
@@ -4,7 +4,14 @@ public abstract class UnaryExpression implements Expression {
 
     protected Expression argument;
 
+    public UnaryExpression() {}
     public UnaryExpression(Expression argument) {
+        this();
+        this.argument = argument;
+    }
+
+    // TODO potential null pointer exception bug must be handled (think of an optional)
+    public void setArgument(Expression argument) {
         this.argument = argument;
     }
 
